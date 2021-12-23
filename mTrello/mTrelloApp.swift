e59aca0fda0d88bcdc1a816ec559114e26ef5c16
Doug Robison <dfrobison@gmail.com>
2021-12-23 16:28:58 -0700

    Initial Commit

---
+++ mTrello/mTrelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  mTrelloApp.swift
+//  mTrello
+//
+//  Created by Doug on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct mTrelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
